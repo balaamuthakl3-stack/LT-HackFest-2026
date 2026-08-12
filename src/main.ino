56949e6c9a47a92b1e5294ecd4c7ce4d50c10fe3
balaamuthakl3-stack <balaamuthakl3@gmail.com>
2026-08-12 22:41:19 +0530

    Create main.ino

---
+++ src/main.ino
@@ -0,0 +1,14 @@
+// Smart Toxic Gas Early-Warning & Worker Safety System
+// LT HackFest 2026
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Smart Toxic Gas Safety System");
+  Serial.println("ESP32 Started Successfully");
+}
+
+void loop() {
+  Serial.println("System Monitoring...");
+  delay(2000);
+}
