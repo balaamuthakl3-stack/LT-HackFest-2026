ccad5da82dd94afdc71c06e594474d62c295c4c2
balaamuthakl3-stack <balaamuthakl3@gmail.com>
2026-08-12 22:44:58 +0530

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,14 +1,35 @@
 // Smart Toxic Gas Early-Warning & Worker Safety System
 // LT HackFest 2026
 
+// Smart Toxic Gas Early-Warning & Worker Safety System
+// LT HackFest 2026
+
+const int GAS_SENSOR_PIN = 34;
+
 void setup() {
   Serial.begin(115200);
 
   Serial.println("Smart Toxic Gas Safety System");
   Serial.println("ESP32 Started Successfully");
+
+  pinMode(GAS_SENSOR_PIN, INPUT);
 }
 
 void loop() {
-  Serial.println("System Monitoring...");
+
+  int gasValue = analogRead(GAS_SENSOR_PIN);
+
+  Serial.print("Gas Sensor Value: ");
+  Serial.println(gasValue);
+
+  if (gasValue > 2000) {
+    Serial.println("WARNING: Abnormal Gas Level Detected!");
+  }
+  else {
+    Serial.println("Status: SAFE");
+  }
+
+  Serial.println("----------------------");
+
   delay(2000);
 }
